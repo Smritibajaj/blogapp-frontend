c1c307d986ae2eba7425686adc17a8dc84a3ee9b
simmy bajaj <simmybajaj1603@gmail.com>
2022-02-21 10:55:38 +0530

    blog app frontend

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -4,7 +4,7 @@ export const API_URL = {
     CREATE_BLOG: `${BACKEND_URL}/v1/blog/create`,
     UPDATE_BLOG:`${BACKEND_URL}/v1/blog/update`,
     GET_ALL_BLOG: `${BACKEND_URL}/v1/blog/allblogs`,
-    DELETE_BLOG: '${BACKEND_URL}/v1/blog/delete',
+    DELETE_BLOG: `${BACKEND_URL}/v1/blog/delete`,
     LOGIN:`${BACKEND_URL}/v1/auth/signin`,
     SIGNUP:`${BACKEND_URL}/v1/auth/signup`,
     GET_USER:`${BACKEND_URL}/v1/user/me`,
